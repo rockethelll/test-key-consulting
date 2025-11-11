f6a6219ef9299edef905e9fd73c80309604d3de1
rockethelll <rockethell@hotmail.com>
2025-11-11 17:33:51 +0100

    Update environment configurations and dependencies; add development environment file, and include testing libraries.

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+
+  regionsUrl: (nom?: string) =>
+    `https://geo.api.gouv.fr/regions${
+      nom ? `?nom=${encodeURIComponent(nom)}` : ''
+    }`,
+
+  departementsUrl: (codeRegion: string) =>
+    `https://geo.api.gouv.fr/regions/${codeRegion}/departements`,
+
+  communesUrl: (codeDepartement: string) =>
+    `https://geo.api.gouv.fr/departements/${codeDepartement}/communes`,
+};
